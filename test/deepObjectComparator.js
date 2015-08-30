f1743055e52e888be21db7f85450fc3b9cd8f39d
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-30 07:51:36 -0700

    test cases for deep object comparator

--- test/deepObjectComparator.js
+++ test/deepObjectComparator.js
@@ -60,6 +60,53 @@ describe('Deep Object comparator', function() {
 
         });
 
+        it('should compare two deep objects successfully where object members are in different order', function() {
+
+            var nestedObj1 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        mother : "Emmy",
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        }
+                    }
+                },
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US',
+                age : 29
+            };
+
+            var nestedObj2 = {
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US',
+                location: ['India', 'US']
+            };
+
+            var list1 = deepComparator.flattenObject(nestedObj1, []);
+            var list2 = deepComparator.flattenObject(nestedObj2, []);
+
+            assert.equal(deepComparator.compareFlattenedArray(list1, list2), true);
+
+        });
+
     });
 
     describe('flat object comparison', function() {
@@ -230,12 +277,217 @@ describe('Deep Object comparator', function() {
             var flattenedObj2 = deepComparator.flattenObject(nestedObj2, []);
 
             assert.equal(deepComparator.compareFlattenedArray(flattenedObj1, flattenedObj2), false);
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj2, flattenedObj1), false);
+
+        });
+
+        it('should return false while comparing unequal objects', function() {
+
+            var nestedObj1 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US'
+            };
+
+            var nestedObj2 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                state : 'CA',
+                country : 'US'
+            };
+
+            var flattenedObj1 = deepComparator.flattenObject(nestedObj1, []);
+            var flattenedObj2 = deepComparator.flattenObject(nestedObj2, []);
+
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj1, flattenedObj2), false);
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj2, flattenedObj1), false);
+
+        });
+
+        it('should return false while comparing unequal objects', function() {
+
+            var nestedObj1 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US'
+            };
+
+            var nestedObj2 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                state : 'CA',
+                country : 'US',
+                city : 'San Jose'
+            };
+
+            var flattenedObj1 = deepComparator.flattenObject(nestedObj1, []);
+            var flattenedObj2 = deepComparator.flattenObject(nestedObj2, []);
+
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj1, flattenedObj2), false);
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj2, flattenedObj1), false);
+
+        });
+
+        it('should return false while comparing unequal objects', function() {
+
+            var nestedObj1 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US'
+            };
+
+            var nestedObj2 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                state : 'CA',
+                country : 'US',
+                city : 'San Jose'
+            };
+
+            var flattenedObj1 = deepComparator.flattenObject(nestedObj1, []);
+            var flattenedObj2 = deepComparator.flattenObject(nestedObj2, []);
+
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj1, flattenedObj2), false);
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj2, flattenedObj1), false);
+
+        });
+
+        it('should return false while comparing unequal objects', function() {
+
+            var nestedObj1 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'foo',
+                            b : 'bar'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                city : 'San Jose',
+                state : 'CA',
+                country : 'US'
+            };
+
+            var nestedObj2 = {
+                location: ['India', 'US'],
+                name : {
+                    firstName : 'Ricky',
+                    lastName : "Travolta",
+                    parents : {
+                        father : {
+                            a : 'a',
+                            b : 'b'
+                        },
+                        mother : "Emmy"
+                    }
+                },
+                age : 29,
+                state : 'CA',
+                country : 'US',
+                city : 'San Jose'
+            };
+
+            var flattenedObj1 = deepComparator.flattenObject(nestedObj1, []);
+            var flattenedObj2 = deepComparator.flattenObject(nestedObj2, []);
+
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj1, flattenedObj2), false);
+            assert.equal(deepComparator.compareFlattenedArray(flattenedObj2, flattenedObj1), false);
+
+        });
+
+
+
+    });
+
+    describe('Bad input', function() {
+
+        it('should throw exception when array is sent as undefined', function() {
+
+            assert.throws(function() {
+                deepComparator.compareFlattenedArray(['abc', 'yyz'], undefined);
+            }, /Can't proceed comparison with undefined objects/);
+
+            assert.throws(function() {
+                deepComparator.compareFlattenedArray(undefined, ['abc', 'yyz']);
+            }, Error);
 
         });
 
     });
-
-    //TODO: fix deep comparator equality
 
     /**
      * TODO:
