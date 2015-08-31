6dcc7ab671ef174de76e23ec619f4421d64ce4a6
Prabhash Rathore <prabhashrathore@gmail.com>
2015-08-31 08:33:13 -0700

    function to setup mock server with initial path

--- index.js
+++ index.js
@@ -6,7 +6,23 @@
 
 var mockServer = require('./lib/mockServer');
 
+
+function createMockServer(options) {
+
+    var Config = require('./lib/config');
+    var config = new Config();
+
+    config.setConfig(options);
+
+    var configData1 = config.getConfig();
+
+    console.log('Rules Path: ' + configData1.rulesPath);
+    console.log('Data Path: ' + configData1.dataPath);
+
+}
+
 module.exports = {
+    setupMockServer: createMockServer,
     getMockData: mockServer.getMockData,
     getMockDataForNestedReq: mockServer.getMockDataForNestedReq
 };
