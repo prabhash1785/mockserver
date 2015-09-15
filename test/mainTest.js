8e5d419096b6b764ce0a65dbae41588bcefc9540
Prabhash Rathore <prabhashrathore@gmail.com>
2015-09-15 06:31:19 -0700

    Added before block and fixed assertions

--- test/mainTest.js
+++ test/mainTest.js
@@ -1,5 +1,5 @@
 /**
- * End to End test test.
+ * End to End test cases
  *
  * Created by prrathore on 8/31/15.
  */
@@ -13,7 +13,19 @@ describe('End to end flow test', function() {
 
     describe('test mock server with default configuration', function() {
 
-        var mockServer = new MockServer();
+        var mockServer;
+
+        before('create mock server instance with default configuration and also reset config object values', function() {
+
+            var config = require('../lib/config');
+            config.setConfig({
+                rulesPath: '/rules',
+                dataPath: '/data'
+            });
+
+            mockServer = new MockServer();
+
+        });
 
         it('should return successful response for flat level request', function() {
 
@@ -30,6 +42,7 @@ describe('End to end flow test', function() {
             };
 
             var response = mockServer.getMockData(apiContext, req);
+            console.log('Response: ' + JSON.stringify(response, null, 4));
 
             assert.equal(response.customerID, '12345');
             assert.equal(response.productCode, 'abcde');
@@ -75,14 +88,20 @@ describe('End to end flow test', function() {
 
     });
 
-    describe('test mock server with default configuration', function() {
+    describe('test mock server with custom configuration', function() {
 
-        var config = {
-            rulesPath: '',
-            dataPath: ''
-        };
+        var mockServer;
 
-        var mockServer = new MockServer(config);
+        before('create mock server instance with custom configuration', function() {
+
+            var config = {
+                rulesPath: 'data/rules/rules',
+                dataPath: 'data/mockResponse'
+            };
+
+            mockServer = new MockServer(config);
+
+        });
 
         it('should return successful response for flat level request', function() {
 
@@ -100,15 +119,24 @@ describe('End to end flow test', function() {
 
             var response = mockServer.getMockData(apiContext, req);
 
-            assert.equal(response.customerID, '12345');
-            assert.equal(response.productCode, 'abcde');
-            assert.equal(response.customer.firstName, 'Foo');
-            assert.equal(response.customer.lastName, 'Bar');
-            assert.equal(response.customer.address.line1, '2121 N First St');
-            assert.equal(response.customer.address.line2, 'Site 45');
-            assert.equal(response.customer.address.city, 'Campbell');
-            assert.equal(response.customer.address.state, 'CA');
-            assert.equal(response.customer.address.zipCode, 95008);
+            assert.equal(response.statusCode, 200);
+            assert.equal(response.preferences.id, 'ab67ws8');
+            assert.equal(response.preferences.accountNumber, '734567');
+            assert.equal(response.preferences.paymentMethod, 'Card');
+            assert.equal(response.preferences.contactMethod, 'email');
+            assert.equal(response.preferences.customer.firstName, 'Iron');
+            assert.equal(response.preferences.customer.lastName, 'Man');
+            assert.equal(response.preferences.customer.address.line1, '2121 N First St');
+            assert.equal(response.preferences.customer.address.line2, 'Site 45');
+            assert.equal(response.preferences.customer.address.city, 'Campbell');
+            assert.equal(response.preferences.customer.address.state, 'CA');
+            assert.equal(response.preferences.customer.address.zipCode, 95008);
+
+            //assert HATEOAS links
+            assert.equal(response.preferences.links[0].rel, 'self');
+            assert.equal(response.preferences.links[0].href, 'https://somerandomcompany.com/accountPreferences');
+            assert.equal(response.preferences.links[1].rel, 'next');
+            assert.equal(response.preferences.links[1].href, 'https://somerandomcompany.com/accountPreference?pageID=2');
 
         });
 
